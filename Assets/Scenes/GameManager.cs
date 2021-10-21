c12a15a191fb1365efccd703df98bcfc4ec2f0bb
Rohel <pereirarohel21@gmail.com>
2021-10-21 16:18:59 -0400

    Initial commit

---
+++ Assets/Scenes/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    // Start is called before the first frame update
+    public float RestartDelay = 1f;
+    public GameObject UILEVEL;
+    public void CompleteLevel ()
+    {
+        UILEVEL.SetActive(true);
+        Debug.Log("Level Complete");
+    }
+    public void EndGame()
+    {
+            Debug.Log("Game Over");
+            Invoke("Restart", RestartDelay);
+        
+    }
+        void Restart()
+        {
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+}
---
+++ Assets/Scripts/playermovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    public float forwardforce = 2000f;
+    public float sidewaysforce = 500f; 
+
+
+    // we marked this as "fixed update" because we are using it to mess with physics.
+    void FixedUpdate()
+    {
+        // adds a forward force
+        rb.AddForce(0, 0, forwardforce * Time.deltaTime);
+
+
+        if ( Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysforce * Time.deltaTime , 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysforce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -2.1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+
+    }
+
+}
